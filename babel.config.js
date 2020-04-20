97e83712b6a4a55f446b809313876b6b0df17ac4
liuxiang <949282760@qq.com>
2020-04-20 17:41:13 +0800

    'liuxiang'

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,6 @@
+/*vue的create里只选了router 和baber（用于将js转化为浏览器可识别的js简单地说，
+Babel 能够转译 ECMAScript 2015+ 的代码，使它在旧的浏览器或者环境中也能够运行。）*/
+
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import './plugins/element.js'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 Vue.config.productionTip = false
+Vue.use(ElementUI);
 
 new Vue({
   router,
   render: h => h(App)
+  //Vue对象挂载到div id=app 上，等于el根实例
 }).$mount('#app')
+
