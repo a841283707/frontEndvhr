2def24649a9a5312a3e82e829bcbd2e542ee92e3
Gogs <gogs@fake.local>
2020-06-11 21:18:13 +0800

    基础数据页面更新

--- src/views/EmpBasic.vue
+++ src/views/EmpBasic.vue
@@ -5,18 +5,18 @@
                     border
                     style="width: 100%">
                 <el-table-column
+                        fixed
                         prop="name"
                         label="姓名"
                         width="120">
                 </el-table-column>
                 <el-table-column
-                        fixed
-                        prop="card"
+                        prop="workid"
                         label="工号"
                         width="150">
                 </el-table-column>
                 <el-table-column
-                        prop="sex"
+                        prop="gender"
                         label="性别"
                         width="120">
                 </el-table-column>
@@ -26,27 +26,27 @@
                         width="120">
                 </el-table-column>
                 <el-table-column
-                        prop="ICard"
+                        prop="idcard"
                         label="身份证号码"
-                        width="300">
+                        width="200">
                 </el-table-column>
                 <el-table-column
-                        prop="marry"
+                        prop="wedlock"
                         label="婚姻状况"
                         width="120">
                 </el-table-column>
                 <el-table-column
-                    prop="nation"
+                    prop="nationid"
                     label="民族"
                     width="100">
                 </el-table-column>
                 <el-table-column
-                    prop="homeTown"
+                    prop="nativeplace"
                     label="籍贯"
                     width="100">
                 </el-table-column>
                 <el-table-column
-                    prop="politicalStatus"
+                    prop="politicid"
                     label="政治面貌"
                     width="100">
                 </el-table-column>
@@ -56,39 +56,40 @@
                         width="200">
                 </el-table-column>
                 <el-table-column
-                        prop="phoneNumber"
+                        prop="phone"
                         label="电话号码"
                         width="150">
                 </el-table-column>
                 <el-table-column
-                        prop="location"
+                        prop="address"
                         label="联系地址"
+                        width="200">
+                </el-table-column>
+                <el-table-column
+                        prop="departmentid"
+                        label="部门"
                         width="150">
                 </el-table-column>
                 <el-table-column
-                        prop="location"
-                        label="联系地址"
+                        prop="joblevelid"
+                        label="职业"
                         width="150">
                 </el-table-column>
                 <el-table-column
-                        prop="location"
-                        label="联系地址"
+                        prop="posid"
+                        label="职称"
                         width="150">
                 </el-table-column>
                 <el-table-column
-                        prop="location"
-                        label="联系地址"
-                        width="150">
-                </el-table-column><el-table-column
-                    prop="location"
-                    label="联系地址"
+                    prop="engageform"
+                    label="合同"
                     width="150">
-                </el-table-column><el-table-column
-                        prop="location"
-                        label="联系地址"
+                </el-table-column>
+                <el-table-column
+                        prop="tiptopdegree"
+                        label="学历"
                         width="150">
                 </el-table-column>
-
 
                 <el-table-column
                         fixed="right"
@@ -101,24 +102,50 @@
                 </el-table-column>
             </el-table>
             <el-pagination
+                    :page-sizes="[10, 15, 20, 25]"
+                    :page-size="size"
+                    @size-change="handleSizeChange"
+                    @current-change="handleCurrentChange"
                     style="display: flex;justify-content: flex-end"
                     background
-                    layout="prev, pager, next"
+                    layout="sizes ,prev, pager, next"
                     :total="1000">
             </el-pagination>
     </div>
 </template>
 
 <script>
+    import {getRequest} from "../networks/request";
+    import {Message} from 'element-ui'
     export default {
         name: "EmpBasic",
         methods: {
             handleClick(row) {
                 console.log(row);
+            },
+            getInitEmp(){
+                getRequest("/employee/basic/",{
+                    page: 1,
+                    size: 9
+                }).then(res=>{
+                    this.tableData=res.data;
+                }).catch(error=>{
+                    Message.error("出现错误")
+                })
+            },
+            handleSizeChange(val){
+                console.log(val);
+            },
+            handleCurrentChange(val){
+                console.log(val);
             }
+        },
+        created(){
+          this.getInitEmp();
         },
         data() {
             return {
+                size: 9,
                 tableData: [{
                     date: '2016-05-02',
                     name: '王小虎',
@@ -149,7 +176,7 @@
                     zip: 200333
                 }]
             }
-        }
+        },
     }
 </script>
 
